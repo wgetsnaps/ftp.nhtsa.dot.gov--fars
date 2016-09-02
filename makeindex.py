6c1de6275e98b14a658b241d5fcab9b02045d3cc
Dan Nguyen <dansonguyen@gmail.com>
2016-09-01 17:30:21 -0700

    index

--- makeindex.py
+++ makeindex.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+"""
+Prints web browser friendly index of the contents of the fars/ directory
+to stdout
+"""
+
+
 from re import compile
 from subprocess import Popen, PIPE
 from sys import stdout
@@ -5,9 +13,18 @@ from urllib.parse import quote
 
 DEPTH_RX = compile(r'[└├│]')
 STEM_RX = compile(r'^(.*?── )(.+)')
-
 URL_ROOT = 'https://wgetsnaps.github.io/ftp.nhtsa.dot.gov--fars/'
-stdout.write("\n<pre>\n")
+
+
+stdout.write("""
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>NHTSA FARS bulk data FTP mirrored as HTML</title>
+<body>
+<a href="https://github.com/wgetsnaps/ftp.nhtsa.dot.gov--fars">See Github repo for details</a>
+
+<pre>
+""")
 
 with Popen(['tree', 'fars'], stdout=PIPE) as proc:
     stack = []
@@ -29,4 +46,4 @@ with Popen(['tree', 'fars'], stdout=PIPE) as proc:
         # https://wgetsnaps.github.io/ftp.nhtsa.dot.gov--fars/fars/FARS-DOC/DEFS2001.zip
         stdout.write("""{0}<a href="{1}">{2}</a>\n""".format(roottxt, url, stem))
 
-stdout.write("</pre>\n")
+stdout.write("\n</pre>\n")
